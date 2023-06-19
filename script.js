c5f046a484a99abf03c4bee289f9f036f089296d
whitok355 <3139139@mail.ru>
2023-06-19 20:24:31 +0300

    fourth hw

--- script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('.form__btn');
-const form = document.querySelector('.form')
-
-const printName = () =>{
-    document.querySelector('.output__name').innerHTML = document.querySelector('.form__input').value;
-}
-
-btn.addEventListener('click', printName)
